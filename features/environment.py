5fe253bad1a77669c982dd23ebb2393f7d3bd026
Alexander Harris <alexander.harris@mail.mcgill.ca>
2019-03-06 16:02:15 -0500

    Added travis

--- features/environment.py
+++ features/environment.py
@@ -1,11 +1,17 @@
 from behave import fixture, use_fixture
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
+DEBUG = False
+
 @fixture
 def browser(context):
-    context.browser = webdriver.Chrome(ChromeDriverManager().install())
+    options = Options()
+    if not DEBUG:
+        options.add_argument('--headless')
+    context.browser = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
     yield context.browser
     context.browser.quit()
 
