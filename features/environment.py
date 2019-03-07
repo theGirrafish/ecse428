f6e934ac552230f8182ec9ba43ee57ba95629807
Alexander Harris <alexander.harris@mail.mcgill.ca>
2019-03-07 17:26:35 -0500

    Increased timeout

--- features/environment.py
+++ features/environment.py
@@ -18,18 +18,20 @@ def browser(context):
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-extensions')
     options.add_argument('--start-maximized')
-    options.add_argument('--window-size=1920,1080')
+    options.add_argument('--no-sandbox')
     if HEADLESS:
         options.add_argument('--headless')
         if DEBUG:
             options.add_argument('--remote-debugging-port=9222')
+    else:
+        options.add_argument('--window-size=1920,1080')
     context.browser = webdriver.Chrome(executable_path=ChromeDriverManager().install(), chrome_options=options)
     # context.browser = webdriver.Firefox(executable_path=GeckoDriverManager().install(), firefox_options=options)
     yield context.browser
     context.browser.quit()
 
 def before_all(context):
-    context.gmail = Gmail(context, timeout=20)
+    context.gmail = Gmail(context, timeout=60)
 
 def before_scenario(context, scenario):
     use_fixture(browser, context)
